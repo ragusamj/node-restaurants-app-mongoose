4cfc85ebfbf535b6859507a731ea645b6ddc4237
Marija Jovanovic <marija.jo@hotmail.com>
2018-02-14 17:54:37 -0500

    newst import

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:admin@ds233228.mlab.com:33228/restaurants-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:admin@ds233228.mlab.com:33228/restaurants-app';
 exports.PORT = process.env.PORT || 8080;
 
 //import
+//mongodb://admin:admin@ds233228.mlab.com:33228/restaurants-app
